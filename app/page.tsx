f079879899eae9b50aa951df4d54710e806f2709
Wellington Galvão <wellingtongalvao96@gmail.com>
2026-03-02 15:10:47 -0300

    feat: aviso de projetos em manutenção

--- app/page.tsx
+++ app/page.tsx
@@ -4,20 +4,27 @@ import { Education } from "./_components/education"
 import { Hero } from "./_components/hero"
 import { MyDock } from "./_components/my-dock"
 import { Projects } from "./_components/projects"
-import { IconSkill } from "@/components/icon-skill"
 import { Skills } from "./_components/skills"
+import { Info } from "lucide-react"
 
 export default function Home() {
   return (
-    <div className="mx-auto flex min-h-dvh w-full max-w-screen-xl flex-col items-center border-x px-4 pt-8 pb-4 sm:pt-16">
-      <Wrapper className="flex-col gap-8">
-        <Hero />
-        <Skills />
-        <Projects />
-        <Education />
-        <Certificates />
-        <MyDock />
-      </Wrapper>
-    </div>
+    <>
+      <div className="bg-destructive flex items-center gap-2 p-1 text-sm text-white">
+        <Info className="size-4" />
+        <p>Sites em Next.Js estão em manutenção</p>
+      </div>
+      <div className="mx-auto flex min-h-dvh w-full max-w-7xl flex-col items-center border-x px-4 pt-8 pb-4 sm:pt-16">
+        <Wrapper className="flex-col gap-8">
+          <Hero />
+          <Skills />
+          <Projects />
+          <Education />
+          <Certificates />
+          <MyDock />
+        </Wrapper>
+      </div>
+      ♫
+    </>
   )
 }
